34125efb5632d05c71321f696a29f021e9e6b068
Romulo Rosa <romulo.frontend@gmail.com>
2023-03-11 21:07:46 -0300

    js carrossel bg

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log("Teste the last of us")
+
+const botoesCarrossel = document.querySelectorAll(".conteudo__carrosselbotoesitem");
+
+const imagens = document.querySelectorAll(".carrossel__imagem");
+
+
+botoesCarrossel.forEach((botao, i) => {
+  botao.addEventListener('click', () =>{
+    
+    desativarBotaoSelecionado();
+
+    selecionarBotaoCarrossel(botao);
+
+    esconderImagemAtiva();
+
+    mostrarImagemFundo(i);
+  })
+})
+
+function desativarBotaoSelecionado() {
+  const botaoSelecionado = document.querySelector(".conteudo__carrosselbotoesitem--selecionado");
+  
+  botaoSelecionado.classList.remove("conteudo__carrosselbotoesitem--selecionado");
+}
+
+function selecionarBotaoCarrossel(botao) {
+  botao.classList.add("conteudo__carrosselbotoesitem--selecionado");
+}
+
+function esconderImagemAtiva() {
+  const imagemAtiva = document.querySelector(".carrossel__imagem--ativa");
+
+  imagemAtiva.classList.remove("carrossel__imagem--ativa");
+}
+
+function mostrarImagemFundo(i) {
+  imagens[i].classList.add("carrossel__imagem--ativa");
+}
